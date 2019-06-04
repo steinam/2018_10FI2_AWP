352372b68450beaecddae87a1d94868ed1477097
steinam <webmaster@steinam.de>
2019-06-04 15:48:54 +0200

    demos zu try catch

---
+++ woche_9_10/delete.ps1
@@ -0,0 +1,63 @@
+﻿
+function auto_delete([string]$FullFilePfad)
+{
+    #String ist null
+    #falscher Pfad = Pfad nicht vorhanden
+    #keine Rechte
+    try{
+        del $FullFilePfad -ErrorAction Stop 
+   
+        write-Host "Steinam ist toller"    
+    }
+
+    catch [System.Management.Automation.ItemNotFoundException]
+    {
+        Write-Host $_  #Fehler
+
+    }
+
+    catch{
+
+    }
+    
+  
+        
+    #Wird auch immer ausgeführt, auch wenn 
+    #kein Fehler erzeugt wird
+    finally{
+        Write-Host "Bin im finally - Block"
+    }
+}
+
+
+auto_delete "C:\temp\test123\html_und_css.docx"
+
+#$Error[0].Exception.GetType().FullName
+#$_
+
+
+#del C:\temp\test\genindex.html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function StartWord()
+{
+    
+    #Ist Word vorhanden ?
+    $Winword = New-Object -ComObject Word.Application
+
+    return $winword
+
+}
+
---
+++ woche_9_10/try_catch.ps1
@@ -0,0 +1,54 @@
+﻿function deleteFile([string]$FullPathOfFile)
+{
+    #Löschen der Datei
+    #Möfliche Probleme
+    #- Datei nicht vorhanden
+    #- Datei ist gelocked
+    #- Keine Löschrechte auf Datei
+    
+    try{ 
+        #Wir probieren die DAtei zu löschen
+        #wenn es funktioniert, dann ist alles gut
+        #wenn nicht, wird durh -ErrorAction STop eine Ausnahme
+        #produziert, die vom catch-Block gefangen werden kann
+    
+        del $FullPathOfFile -ErrorAction Stop
+
+        $Datum = Get-Date
+        $text = "Datum: " + $Datum + ":" + $FullPathOfFile
+        Add-Content -Value $text -Path C:\temp\test\logfile.txt
+
+        write-Host "Steinam ist der Beste"
+
+    }
+
+
+
+    #ich will einen bestimmten Fehlertyp fangen
+    catch [System.Management.Automation.ParameterBindingException]{
+
+        Write-Host "Mache deine Addcontent richtig" 
+    }
+
+
+
+    catch{
+        #Logik im Fehlerfalle
+        Write-Host "Datei konnte nicht gelöscht werden"
+
+    }
+
+    
+    #Loggen des Löschvorgansg in einer Textdatei
+    #mit Angabe von Datum des Löschvorgangs
+
+    #Codeblock, der immer ausgeführt wird, egal ob das Programm einen 
+    #Fehler wirft oder nicht
+    finally{
+
+
+    }
+}
+
+
+deleteFile "C:\temp\test\Hotelzimmer.docx"
